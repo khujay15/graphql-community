0ecfb76302c28e58d4fd5cca38b5b1a844fdc07b
jaehyuk-jang <jaehyuk.jang@navercorp.com>
2021-06-08 01:17:45 +0900

    Fix backend url

--- project/packages/web/lib/apollo.ts
+++ project/packages/web/lib/apollo.ts
@@ -11,7 +11,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "http://localhost:5000/graphql", // Server URL (must be absolute)
+      uri: "http://backend/graphql", // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache({
